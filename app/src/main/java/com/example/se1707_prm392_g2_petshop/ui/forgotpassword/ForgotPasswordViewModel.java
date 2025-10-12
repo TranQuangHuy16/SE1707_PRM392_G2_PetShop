44a1b23ad9726246f866f7eaa59dd6c0e1693b9d
Minh Hoàng <133775517+Yuuzzz123@users.noreply.github.com>
2025-10-12 20:19:06 +0700

    Basic Layout

---
+++ app/src/main/java/com/example/se1707_prm392_g2_petshop/ui/forgotpassword/ForgotPasswordViewModel.java
@@ -0,0 +1,7 @@
+package com.example.se1707_prm392_g2_petshop.ui.forgotpassword;
+
+import androidx.lifecycle.ViewModel;
+
+public class ForgotPasswordViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
