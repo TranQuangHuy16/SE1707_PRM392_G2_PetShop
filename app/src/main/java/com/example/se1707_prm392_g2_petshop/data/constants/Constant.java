d3c0b96686d9d6cc064e59067b7942a96b5b90cc
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2025-11-01 23:34:02 +0700

    fix android manifest

--- app/src/main/java/com/example/se1707_prm392_g2_petshop/data/constants/Constant.java
+++ app/src/main/java/com/example/se1707_prm392_g2_petshop/data/constants/Constant.java
@@ -3,6 +3,8 @@ package com.example.se1707_prm392_g2_petshop.data.constants;
 public class Constant {
     // For Android Emulator - localhost
     public static final String BASE_URL = "http://10.0.2.2:5044/api/";
+//    public static final String BASE_URL = "https://se1707-prm392-g2-petshop-be.onrender.com/api/";
+
     
     // For Real Device - use your PC's local IP address
     // public static final String BASE_URL = "http://192.168.1.x:5044/api/";
