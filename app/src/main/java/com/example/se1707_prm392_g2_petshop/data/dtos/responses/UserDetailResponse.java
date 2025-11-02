414290199605c5189f1ce99ca19ccf4b6baac1eb
khoindSE180124 <khoind2004@gmail.com>
2025-11-02 18:59:33 +0700

    feat(admin/users): implement basic user management UI, adapters, and presenters; apply edge-to-edge insets

---
+++ app/src/main/java/com/example/se1707_prm392_g2_petshop/data/dtos/responses/UserDetailResponse.java
@@ -0,0 +1,4 @@
+package com.example.se1707_prm392_g2_petshop.data.dtos.responses;
+
+public class UserDetailResponse {
+}
---
+++ app/src/main/java/com/example/se1707_prm392_g2_petshop/ui/admin/users/manage/UserManagePresenter.java
@@ -0,0 +1,4 @@
+package com.example.se1707_prm392_g2_petshop.ui.admin.users.manage;
+
+public class UserManagePresenter {
+}
