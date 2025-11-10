d226370ce7dec2d3e50923d1c9e3248316be362c
Minh Hoàng <133775517+Yuuzzz123@users.noreply.github.com>
2025-11-10 15:18:25 +0700

    fix something

--- app/src/main/java/com/example/se1707_prm392_g2_petshop/data/constants/Constant.java
+++ app/src/main/java/com/example/se1707_prm392_g2_petshop/data/constants/Constant.java
@@ -7,9 +7,9 @@ public class Constant {
     // can reach the backend. Using the provided IP 172.20.10.2 and port 5044.
 //   public static final String BASE_URL = "http://10.0.2.2:5044/api/";
 
-//    public static final String BASE_URL = "https://se1707-prm392-g2-petshop-be.onrender.com/api/";
+    public static final String BASE_URL = "https://se1707-prm392-g2-petshop-be.onrender.com/api/";
 
     
     // For Real Device - use your PC's local IP address
-     public static final String BASE_URL = "http://192.168.0.102:5044/api/";
+//     public static final String BASE_URL = "http://192.168.0.x:5044/api/";
 }
